6b21d00cf2b15c0a42649379d4c9e07fb377f2fc
Luis <nanobipede@gmail.com>
2019-07-07 19:06:22 -0300

    Personagem Andando

---
+++ Assets/Personagem.cs
@@ -0,0 +1,10 @@
+
+    public class Personagem
+    {
+        private float velocidade;
+
+        Personagem()
+        {
+            velocidade = 10;
+        }
+    }
--- Assets/control.cs
+++ Assets/control.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngineInternal.Input;
 
 public class control : MonoBehaviour
 {
+    public Rigidbody2D body;
+    
+    public Personagem personagem;
 
     private Animator myAnimator;
     // Start is called before the first frame update
     void Start()
     {
         myAnimator = GetComponent<Animator>();
+
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -17,14 +23,35 @@ public class control : MonoBehaviour
     {
         if (Input.GetAxis("Horizontal") > 0)
         {
-            print(Input.GetAxis("Horizontal"));
+            Vector3 flipar = new Vector3(1,1,1);
+            transform.localScale = flipar;
+            
+            if (body.velocity.magnitude < 4)
+            {
+                body.AddForce(Vector2.right*8);    
+            }
             
             myAnimator.SetTrigger("start run");
+            myAnimator.ResetTrigger("start idle");
             
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            Vector3 flipar = new Vector3(-1,1,1);
+            transform.localScale = flipar;
+            
+            if (body.velocity.magnitude < 4)
+            {
+                body.AddForce(Vector2.left*8);    
+            }
+
+            myAnimator.SetTrigger("start run");
+            myAnimator.ResetTrigger("start idle");  
         }
         else
         {
             myAnimator.SetTrigger("start idle");
+            myAnimator.ResetTrigger("start run");
         }
     }
 }
