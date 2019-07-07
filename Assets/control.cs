713af34a2811e5e24bde6253d536489f9fbbfd44
Bruno Willian Andrade <brunowillianandrade@gmail.com>
2019-07-07 17:24:22 -0300

    first commit

---
+++ Assets/control.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+
+    private Animator myAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            print(Input.GetAxis("Horizontal"));
+            
+            myAnimator.SetTrigger("start run");
+            
+        }
+        else
+        {
+            myAnimator.SetTrigger("start idle");
+        }
+    }
+}
