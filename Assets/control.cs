6bd078114e1bd8e6ce87b3b37240dee95caaf22f
Luis <nanobipede@gmail.com>
2019-07-09 23:12:56 -0300

    Pulo (tentativa 1)

--- Assets/control.cs
+++ Assets/control.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngineInternal.Input;
 
@@ -10,6 +12,9 @@ public class control : MonoBehaviour
     public Personagem personagem;
 
     private Animator myAnimator;
+
+    private float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +26,8 @@ public class control : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ////MOVIMENTO HORIZONTAL
+        
         if (Input.GetAxis("Horizontal") > 0)
         {
             Vector3 flipar = new Vector3(1,1,1);
@@ -53,5 +60,20 @@ public class control : MonoBehaviour
             myAnimator.SetTrigger("start idle");
             myAnimator.ResetTrigger("start run");
         }
+        
+        ////MOVIMENTO VERTICAL
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && Input.GetAxis("Vertical") < 0.07)
+        {
+            jumpForce = 230;
+            body.AddForce(Vector2.up * jumpForce);
+
+        }
+
+        else
+        {
+            jumpForce = 0;
+        }
+
     }
 }
